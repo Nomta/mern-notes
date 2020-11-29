daea54f412348807a2730152b94e1d44e9b486b7
Nomta <nomta@mail.ru>
2020-11-29 13:20:54 +0300

    db connection

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const config = require('config');
+const mongoose = require('mongoose');
 const app = require('./app');
-const config = require('./config');
 
-app.listen(config.port, () => {
-  console.log(`App is running on http://localhost:${config.port}`)
-});
+const PORT = config.get('port') || 7000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongodb').uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        app.listen(PORT, () => {
+          console.log(`App has been running on http://localhost:${PORT}`)
+        });
+    } 
+    catch(err) {
+        console.error('Server error', err.message);
+        process.exit(1);
+    }
+}
+
+start();
