b14c502153535532d6a8a6f9aebf2381e587ec14
Nomta <nomta@mail.ru>
2020-12-04 03:25:02 +0300

    env

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.json({extended: true}))
+app.use(express.json({ extended: true }));
 
-app.use('/api/auth', require('./routes/auth'))
+app.use("/api/auth", require("./routes/auth"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 module.exports = app;
